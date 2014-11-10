03281acab325c8ec505c55593443cd932da1ab5d
Steve Kinney <hello@stevekinney.net>
2014-11-10 11:17:32 -0700

    Implement pubsub

--- public/ideabox.js
+++ public/ideabox.js
@@ -2,17 +2,24 @@ $ideaText = $('#idea-text');
 $ideaSubmit = $('#idea-submit');
 $ideaList = $('#ideas');
 
+var client = new Faye.Client('/faye');
+var subscription = client.subscribe('/ideas', function(idea) {
+  addIdea(idea);
+});
+
 $ideaSubmit.on('click', function (e) {
   e.preventDefault();
   var idea =  { idea: $ideaText.val(), status: 'incomplete' };
   $.post('/idea.json', idea)
-    .done(addIdea)
+    .done(function (data) {
+      client.publish('/ideas', data);
+    })
     .fail(function () {
       alert('Oh noes!');
     });
 });
 
 function addIdea(data) {
-  data = JSON.parse(data);
+  if (typeof data === 'string') data = JSON.parse(data);
   $ideaList.append('<li class="idea">' + data.idea + '</li>')
 }
