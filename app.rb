edb4c6175675647091794d9676f6459c2a43a630
Steve Kinney <hello@stevekinney.net>
2014-11-10 11:12:24 -0700

    Basic request and response

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
+require 'json'
+
 class IdeaBox < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+  
+  post '/idea.json' do
+    # Make believe we store it in a database and validate it or whatnot.
+    params.to_json
   end
 
   run! if app_file == $0
--- public/ideabox.js
+++ public/ideabox.js
@@ -1,6 +1,18 @@
 $ideaText = $('#idea-text');
 $ideaSubmit = $('#idea-submit');
+$ideaList = $('#ideas');
 
-$ideaSubmit.on('click', function () {
-  alert($ideaText.val());
+$ideaSubmit.on('click', function (e) {
+  e.preventDefault();
+  var idea =  { idea: $ideaText.val(), status: 'incomplete' };
+  $.post('/idea.json', idea)
+    .done(addIdea)
+    .fail(function () {
+      alert('Oh noes!');
+    });
 });
+
+function addIdea(data) {
+  data = JSON.parse(data);
+  $ideaList.append('<li class="idea">' + data.idea + '</li>')
+}
